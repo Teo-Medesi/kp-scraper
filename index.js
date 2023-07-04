ee0413f74e3c33df60ca71263054f1c8aace6f28
Teo-Medesi <teo.medesi@gmail.com>
2023-07-04 11:31:21 +0200

    added getListing() method to kp class

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 import KupujemProdajem from "./kp-scraper.js";
-import { saveToFolder } from "./utils/utils.js";
 
 const run = async () => {
   const kp = new KupujemProdajem();
   await kp.init();
 
-  const categories = await kp.getCategories();
-  const tools = await categories.getCategory("Alati i oruđa");
-
-  const listings = await tools.getListings();
-
-  const url = "https://novi.kupujemprodajem.com/alati-i-orudja/elektricni/masina-za-sisanje-ovaca-ruska-800w-nova/oglas/150762602?filterId=2139960553"
-  const listing = await listings.getListing(url);
-
-  const images = await listing.getImages();
-  saveToFolder(images, "json/images.json");
+  // run code here
 
   await kp.close();
 }
